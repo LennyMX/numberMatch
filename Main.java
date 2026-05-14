c63f738c24ac643fbb5964626d643e15c8100e3e
LennyMX <patron.luis@uabc.edu.mx>
2026-05-13 23:05:15 -0700

    Create Main class to launch JavaFX application

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.example.numbermatch;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Application.launch(Interfaz.class, args);
+
+    }
+}
